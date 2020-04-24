fdc31b80a26a632c6a0350d9789b33ce4cfd665b
Your Name <you@example.com>
2020-04-24 23:08:11 +0700

    hh

--- static/update.js
+++ static/update.js
@@ -1,8 +1,8 @@
-dtb.ref('/heart_rate/temp_heart').on('value',snap => document.getElementById("heart_rate").innerHTML = snap.val());
+dtb.ref('/heart_rate/temp_heart').on('value',snap => {if(snap.val()!==0) document.getElementById("heart_rate").innerHTML = snap.val();});
 
 dtb.ref('/temperature/temperature_1').on('value',snap => document.getElementById("temperature").innerHTML = snap.val());
 
-dtb.ref('/temperature/stress_1').on('value',snap => document.getElementById("stress").innerHTML = snap.val());
+dtb.ref('/stress/stress_1').on('value',snap => document.getElementById("stress").innerHTML = snap.val());
 
 //send to firebase: %stress
 var stress_1_value=0;
@@ -13,9 +13,9 @@ dtb.ref('/stress/').once('value')
     obj_value = snapshot.val();
   }).then(response => {
         firebase.database().ref('stress/').set({
-            stress_3: obj_value["stress_2"],
-            stress_2: obj_value["stress_1"],
-            stress_1 : obj_value["stress_1"]+5
+         //   stress_3: obj_value["stress_2"],
+           // stress_2: obj_value["stress_1"],
+           // stress_1 : obj_value["stress_1"]+5
           })});
 
  
