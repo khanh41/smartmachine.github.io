f4859371abfdf4b0f25f974bf7e6251af0cdee21
Your Name <you@example.com>
2020-04-24 23:18:44 +0700

    zxc

--- static/update.js
+++ static/update.js
@@ -4,18 +4,4 @@ dtb.ref('/temperature/temperature_1').on('value',snap => document.getElementById
 
 dtb.ref('/stress/stress_1').on('value',snap => document.getElementById("stress").innerHTML = snap.val());
 
-//send to firebase: %stress
-var stress_1_value=0;
-var stress_2_value=0; 
-var obj_value;
-dtb.ref('/stress/').once('value')
-  .then(snapshot => {
-    obj_value = snapshot.val();
-  }).then(response => {
-        firebase.database().ref('stress/').set({
-         //   stress_3: obj_value["stress_2"],
-           // stress_2: obj_value["stress_1"],
-           // stress_1 : obj_value["stress_1"]+5
-          })});
-
  
