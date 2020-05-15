2820c8beeef9ace55facd86031ee461cc24f97a3
Your Name <you@example.com>
2020-05-16 01:49:25 +0700

    zxc

--- static/dchart.js
+++ static/dchart.js
@@ -1,41 +1,25 @@
-function displayLineChart() {
-    var data = {
-        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        datasets: [
-            {
-                label: "heart",
-                fillColor: "rgba(255,255,255,0)",
-                strokeColor: "rgba(249,93,83,1)",
-                pointColor: "rgba(249,93,83,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(249,93,83,1)",
-        		borderWidth:10,
-                data: [20, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-            },
-            {
-                label: "temp",
-                fillColor: "rgba(255,255,255,0)",
-                strokeColor: "rgba(185,37,218,1)",
-                pointColor: "rgba(185,37,218,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(185,37,218,1)",
-                data: [10, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-            },
-                 {
-                label: "stress",
-                fillColor: "rgba(255,255,255,0)",
-                strokeColor: "rgba(15,120,255,1)",
-                pointColor: "rgba(15,120,255,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(0,0,255,1)",
-                data: [0, 1, 10, 2, 3, 5, 80, 13, 21, 34]
-            }
-        ]
-    };
+data_list = []
+dtb.ref('/stress').on('value',snap => {
+   if(data_list !== snap.val()){
+   data_list = snap.val()
+   var data = {
+      labels: ["No stress", "Interruption", "Condition"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
+          data: [data_list['no_stress'],data_list["interruption"],data_list["condi"]]
+        }
+      ]
+    }
+    var options= {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'Predicted stress in (date)'
+      }
+    }
     var ctx = document.getElementById("lineChart").getContext("2d");
-    var options = {};
-    var lineChart = new Chart(ctx).Line(data, options);
-  }
+    var lineChart = new Chart(ctx).Bar(data, options);
+   }
+});
--- static/update.js
+++ static/update.js
@@ -1,7 +1,5 @@
 dtb.ref('/heart_rate/temp_heart').on('value',snap => {if(snap.val()!==0) document.getElementById("heart_rate").innerHTML = snap.val();});
 
-dtb.ref('/temperature/temperature_1').on('value',snap => document.getElementById("temperature").innerHTML = snap.val());
-
-dtb.ref('/stress/stress_1').on('value',snap => document.getElementById("stress").innerHTML = snap.val());
+dtb.ref('/stress/temp_stress').on('value',snap => document.getElementById("stress").innerHTML = snap.val());
 
  
