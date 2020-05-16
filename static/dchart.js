a57dc0a4d0104ccca3cf4539bea7bd1cda287a55
Your Name <you@example.com>
2020-05-16 09:44:03 +0700

    zxc

--- static/dchart.js
+++ static/dchart.js
@@ -8,7 +8,7 @@ dtb.ref('/stress').on('value',snap => {
         {
           label: "Population (millions)",
           backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
-          data: [data_list['no_stress'],data_list["interruption"],data_list["condi"]]
+          data: [data_list['no_stress'],data_list["interruption"],data_list["time_pressure"]]
         }
       ]
     }
