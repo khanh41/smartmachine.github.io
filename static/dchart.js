4418376eb9e8d7967457508fbdf17357a9a77019
khanh41 <tranvankhanh99a@gmail.com>
2020-05-26 00:19:28 +0700

    final

--- static/dchart.js
+++ static/dchart.js
@@ -4,6 +4,7 @@ stress_chart();
 function selectFunc(){
     var selectBox = document.getElementById("selectBox");
     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+	document.getElementById("details").style.visibility = "visible";
     if(selectedValue==="1"){
 		stress_chart();
 	}
@@ -15,115 +16,140 @@ function selectFunc(){
 
 function stress_2_chart(){
 	data_list = []
-	dtb.ref('/stress').once('value',snap => {
+	dtb.ref('/stress/').once('value',snap => {
    if(data_list !== snap.val()){
-   data_list = snap.val()
-   var data = {
-      labels: ["No stress", "Interruption", "Condition"],
+   data_stress = snap.val();
+   data_itr =[]
+	data_no =[]
+	data_tp =[]
+	for(var i=2;i>=0;i--){
+		data_itr[i]=data_stress["stress"+i]["interruption"]
+		data_no[i]=data_stress["stress"+i]["no_stress"]
+		data_tp[i]=data_stress["stress"+i]["time_pressure"]
+	}
+   new Chart(document.getElementById("lineChart"), {
+    type: 'bar',
+    data: {
+      labels: ["2 days", "1 day", "now"],
       datasets: [
         {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
-          data: [data_list['no_stress'],data_list["interruption"],data_list["time_pressure"]]
+          label: "NS",
+          backgroundColor: "#0000FF",
+          data: data_no
+        }, {
+          label: "ITR",
+          backgroundColor: "#F95D53",
+          data: data_itr
+        },{
+          label: "TPS",
+          backgroundColor: "#B925DA",
+          data: data_tp
         }
       ]
-    }
-    var options= {
-      legend: { display: false },
-      title: {
-        display: true,
-        text: 'Predicted stress in (date)'
-      }
-    }
-    var ctx = document.getElementById("lineChart").getContext("2d");
-    var lineChart = new Chart(ctx).Bar(data, options);
-   }
+    },
+    options: {
+		legend: {
+        display: false
+    },
+		scales: {
+                xAxes: [{
+                    id: 'x-axis-1',
+                }],
+                yAxes: [{
+                    id: 'y-axis-1',
+                    ticks: {beginAtZero: true}
+                }]
+            }                
+        }
+});}
 });
 }
 function stress_chart() {
+	
+	document.getElementById("details").style.visibility = "visible";
 	document.getElementById("selectBox").value=1;
-	dtb.ref('/stress').once('value',snap => {
-    var data = {
-        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        datasets: [
-            {
-                label: "heart",
-                fillColor: "rgba(255,255,255,0)",
-                strokeColor: "rgba(249,93,83,1)",
-                pointColor: "rgba(249,93,83,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(249,93,83,1)",
-        		borderWidth:10,
-                data: [20, 3, 5, 7, 101, 130, 170, 19, 23, 29]
-            },
-            {
-                label: "temp",
-                fillColor: "rgba(255,255,255,0)",
-                strokeColor: "rgba(185,37,218,1)",
-                pointColor: "rgba(185,37,218,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(185,37,218,1)",
-                data: [10, 1, 1, 2, 30, 50, 8, 13, 21, 34]
-            },
-                 {
-                label: "stress",
-                fillColor: "rgba(255,255,255,0)",
-                strokeColor: "rgba(15,120,255,1)",
-                pointColor: "rgba(15,120,255,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(0,0,255,1)",
-                data: [0, 1, 10, 2, 3, 5, 80, 13, 21, 34]
-            }
-        ]
-    };
-    var ctx = document.getElementById("lineChart").getContext("2d");
-    var options = {};
-    var lineChart = new Chart(ctx).Line(data, options);
+	dtb.ref('/line').once('value',snap => {
+	data_stress = snap.val();
+	data_itr =[]
+	data_no =[]
+	data_tp =[]
+	for(var i=9;i>=0;i--){
+		data_itr[i]=data_stress[i+1]["itr"]
+		data_no[i]=data_stress[i+1]["no_stress"]
+		data_tp[i]=data_stress[i+1]["tp"]
+	}
+    var ctx = new Chart(document.getElementById("lineChart"), {
+  type: 'line',
+  data: {
+    labels: [5,"-",4,"-",3,"-",2,"-",1,"-"],
+    datasets: [{ 
+        data: data_no,
+        label: "NS",
+        borderColor: "#0000FF",
+		backgroundColor:"#0000FF",
+        fill: false
+      }, { 
+        data: data_itr,
+        label: "ITR",
+        borderColor: "#F95D53",
+		backgroundColor:"#F95D53",
+        fill: false
+      }, { 
+        data: data_tp,
+        label: "TPS",
+        borderColor: "#B925DA",
+		backgroundColor:"#B925DA",
+        fill: false
+      }
+    ]
+  },
+  options: {
+	  legend: {
+        display: false
+    },
+	  maintainAspectRatio: false,
+    title: {
+      display: true,
+      text: '5 hours ago'
+    }
+  }
+});
 });}
 
 //heart rate chart
 function heart_chart() {
+	document.getElementById("details").style.visibility = "hidden";
 	document.getElementById("selectBox").value=0;
-    var data = {
-        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        datasets: [
-            {
-                label: "heart",
-                fillColor: "rgba(255,255,255,0)",
-                strokeColor: "rgba(249,93,83,1)",
-                pointColor: "rgba(249,93,83,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(249,93,83,1)",
-        		borderWidth:10,
-                data: [20, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-            },
-            {
-                label: "temp",
-                fillColor: "rgba(255,255,255,0)",
-                strokeColor: "rgba(185,37,218,1)",
-                pointColor: "rgba(185,37,218,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(185,37,218,1)",
-                data: [10, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-            },
-                 {
-                label: "stress",
-                fillColor: "rgba(255,255,255,0)",
-                strokeColor: "rgba(15,120,255,1)",
-                pointColor: "rgba(15,120,255,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(0,0,255,1)",
-                data: [0, 1, 10, 2, 3, 5, 80, 13, 21, 34]
-            }
-        ]
-    };
-    var ctx = document.getElementById("lineChart").getContext("2d");
-    var options = {};
-    var lineChart = new Chart(ctx).Line(data, options);
-}
+	dtb.ref('/heart_rate').once('value',snap => {
+	heart_data = snap.val();
+	heart_data=heart_data.map(parseFloat).reverse();
+	labels=[];
+	labels[0]=2;
+	labels[heart_data.length-1]=0;
+	for(var i=1;i<heart_data.length-1;i++){
+		labels[i]='-';
+	}
+    var ctx = new Chart(document.getElementById("lineChart"), {
+  type: 'line',
+  data: {
+    labels: labels,
+    datasets: [{ 
+        data: heart_data,
+        borderColor: "#F95D53",
+		backgroundColor:"#F95D53",
+        fill: false
+      }
+    ]
+  },
+  options: {
+	  legend: {
+        display: false
+    },
+	  maintainAspectRatio: false,
+    title: {
+      display: true,
+      text: '2 minutes ago'
+    }
+  }
+});
+});}
