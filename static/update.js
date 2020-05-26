a76fd07c8cd9f39e68e28029439a81229e5bf7bf
khanh41 <tranvankhanh99a@gmail.com>
2020-05-26 09:22:32 +0700

    final

--- static/update.js
+++ static/update.js
@@ -1,5 +1,5 @@
-dtb.ref('/heart_rate/temp_heart').on('value',snap => {if(snap.val()!==0) document.getElementById("heart_rate").innerHTML = snap.val();});
+dtb.ref('/heart_rate/0').on('value',snap => {if(snap.val()!==0) document.getElementById("heart_rate").innerHTML = snap.val();});
 
-dtb.ref('/stress/temp_stress').on('value',snap => document.getElementById("stress").innerHTML = snap.val());
+dtb.ref('/stress_now').on('value',snap => document.getElementById("stress").innerHTML = snap.val());
 
  
