aeea0eb683f1f29d01c2d5ac27dc3389a74cb806
khanh41 <tranvankhanh99a@gmail.com>
2020-06-05 16:57:00 +0700

    fn

--- static/update.js
+++ static/update.js
@@ -1,6 +1,8 @@
 list_hr = []
 list_str = []
-dtb.ref('/line').on('value',snap => {if(snap.val()!=list_hr) {
+list_line =[]
+dtb.ref('/line').on('value',snap => {if(snap.val()!=list_line) {
+	list_line = snap.val();
 	if(check_char) selectFunc();
 	else heart_chart();
 	dtb.ref('/heart_rate').once('value',snap => {
@@ -16,6 +18,7 @@ dtb.ref('/stress').on('value',snap => {if(snap.val()!=list_str) {
 	if(check_char) selectFunc();
 	else heart_chart();
 	dtb.ref('/heart_rate').once('value',snap => {
+		list_hr = snap.val();
 		document.getElementById("heart_rate").innerHTML = list_hr[0];});
 
 	dtb.ref('/stress_now').once('value',snap => document.getElementById("stress").innerHTML = snap.val());
